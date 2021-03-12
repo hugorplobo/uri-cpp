68f6bb23e6c25f1e8993cad4a8777426b0bc58a8
Hugo <hugorplobo@gmail.com>
2021-03-11 21:21:18 -0300

    1035 and 1036

---
+++ beginner/1036.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+
+int main() {
+
+    double a, b, c, delta, x1, x2;
+
+    cin >> a >> b >> c;
+
+    if (a == 0) {
+        cout << "Impossivel calcular" << endl;
+        return 0;
+    }
+
+    delta = pow(b, 2) - 4 * a * c;
+
+    if (delta < 0) {
+        cout << "Impossivel calcular" << endl;
+        return 0;
+    }
+    
+    x1 = (-1 * b + sqrt(delta)) / (2 * a);
+    x2 = (-1 * b - sqrt(delta)) / (2 * a);
+
+    cout << fixed << setprecision(5)
+        << "R1 = " << x1 << endl
+        << "R2 = " << x2 << endl;
+
+}
+
